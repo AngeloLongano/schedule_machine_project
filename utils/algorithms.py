7aa5dba1b4d5f0569a487bb1f6fe498dee7fe25e
AngeloLonganoUni <285288@studenti.unimore.it>
2024-01-25 22:47:57 +0100

    implemented local search

--- utils/algorithms.py
+++ utils/algorithms.py
@@ -1,4 +1,6 @@
+import time
 from utils.data_types import *
+from copy import deepcopy
 
 
 class Job:
@@ -180,6 +182,75 @@ def create_job_to_finish_work(
     return job
 
 
+def insert_move(solution: "Solution") -> "Solution":
+    solution.schedule.sort(key=lambda x: x.remaining_time(), reverse=True)
+    [
+        schedule.list_jobs.sort(key=lambda j: j.time_needed())
+        for schedule in solution.schedule
+    ]
+    modified_solution = deepcopy(solution)
+    for i_s, schedule in enumerate(solution.schedule):
+        for i_job in range(len(schedule.list_jobs)):
+            for i_other_s in range(len(solution.schedule)):
+                if i_s != i_other_s:
+                    is_inserted = modified_solution.insert_job(i_s, i_job, i_other_s)
+
+                    if is_inserted and modified_solution.is_better_solution_than(
+                        solution
+                    ):
+                        return modified_solution
+                    else:
+                        modified_solution = deepcopy(solution)
+
+
+def swap_move(solution: "Solution") -> "Solution":
+    solution.schedule.sort(key=lambda x: x.remaining_time(), reverse=True)
+    [
+        schedule.list_jobs.sort(key=lambda j: j.time_needed())
+        for schedule in solution.schedule
+    ]
+    modified_solution = deepcopy(solution)
+    for i_s_A, schedule in enumerate(solution.schedule):
+        for i_job_A_to_B in range(len(schedule.list_jobs)):
+            for i_s_B in range(len(solution.schedule)):
+                for i_job_B_to_A in range(len(solution.schedule[i_s_B].list_jobs)):
+                    if i_s_A != i_s_B and i_job_A_to_B != i_job_B_to_A:
+                        is_swapped = modified_solution.swap_job(
+                            i_s_A,
+                            i_job_A_to_B,
+                            i_s_B,
+                            i_job_B_to_A,
+                        )
+
+                        if is_swapped and modified_solution.is_better_solution_than(
+                            solution
+                        ):
+                            return modified_solution
+                        else:
+                            modified_solution = deepcopy(solution)
+
+
+def local_search(
+    start_solution,
+    create_job_function: callable = create_job_to_finish_different_works,
+    neighbor_function: callable = insert_move,
+    stop_time_condition: int = 10000,
+):
+    start_solution.constructive_solution(create_job_function)
+    other_solution = deepcopy(start_solution)
+    start_time = time.time()
+    other_solution = deepcopy(neighbor_function(other_solution))
+
+    while (
+        other_solution is not None
+        and not start_solution.is_better_solution_than(other_solution)
+        and time.time() - start_time < stop_time_condition
+    ):
+        start_solution = deepcopy(other_solution)
+        other_solution = deepcopy(neighbor_function(other_solution))
+    return start_solution
+
+
 class Solution:
     def __init__(
         self,
@@ -203,8 +274,68 @@ class Solution:
 
         self.status = "constructed"
 
+    def standard_order(self):
+        self.schedule.sort(key=lambda x: x.machine["name"])
+        [
+            schedule.list_jobs.sort(key=lambda j: j.time_needed(), reverse=True)
+            for schedule in self.schedule
+        ]
+
+    def can_insert_job(
+        self, schedule_from_index: int, job_index: int, schedule_to_index
+    ):
+        schedule_from = self.schedule[schedule_from_index]
+        job = schedule_from.list_jobs[job_index]
+        schedule_to = self.schedule[schedule_to_index]
+        if (
+            schedule_to.get_pallets() >= schedule_from.get_pallets()
+            and schedule_to.remaining_time() >= job.time_needed()
+        ):
+            return True
+        return False
+
+    def insert_job(self, schedule_from_index: int, job_index: int, schedule_to_index):
+        if self.can_insert_job(schedule_from_index, job_index, schedule_to_index):
+            job = self.schedule[schedule_from_index].list_jobs[job_index]
+            self.schedule[schedule_from_index].remove_job(job)
+            self.schedule[schedule_to_index].add_job(job)
+            return True
+        return False
+
+    def swap_job(
+        self,
+        schedule_A_index: int,
+        job_A_to_B_index: int,
+        schedule_B_index,
+        job_B_to_A_index,
+    ):
+        if self.can_insert_job(
+            schedule_A_index, job_A_to_B_index, schedule_B_index
+        ) and self.can_insert_job(schedule_B_index, job_B_to_A_index, schedule_A_index):
+            self.insert_job(schedule_A_index, job_A_to_B_index, schedule_B_index)
+            self.insert_job(schedule_B_index, job_B_to_A_index, schedule_A_index)
+            return True
+        return False
+
     def jobs_used(self):
         return sum([len(s.list_jobs) for s in self.schedule])
 
-    def evaluate(self):
-        pass
+    def remaining_time(self):
+        return sum([s.remaining_time() for s in self.schedule])
+
+    def max_remaining_time(self):
+        return max([s.remaining_time() for s in self.schedule])
+
+    def is_better_solution_than(self, other_solution: "Solution"):
+        return (
+            self.jobs_used() < other_solution.jobs_used()
+            or (
+                self.jobs_used() == other_solution.jobs_used()
+                and self.remaining_time() < other_solution.remaining_time()
+            )
+            or (
+                self.jobs_used() == other_solution.jobs_used()
+                and self.remaining_time() == other_solution.remaining_time()
+                and self.max_remaining_time() > other_solution.max_remaining_time()
+            )
+        )
